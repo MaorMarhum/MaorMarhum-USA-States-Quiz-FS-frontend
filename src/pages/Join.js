bff5c4d5ac15ba1b2c8209866d701b8459dc8fbe
maormarhum <maormarhum9400@gmail.com>
2023-05-30 13:14:04 +0300

    finall

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -5,7 +5,7 @@ import { useHistory } from "react-router-dom";
 const Join = () => {
   const [roomCode, setRoomCode] = useState("");
   const history = useHistory();
-  const isFormValid = roomCode.trim().length === 6
+  const isFormValid = roomCode.trim().length === 6;
 
   const handleSubmit = (event) => {
     event.preventDefault();
